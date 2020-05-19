127968d01b279e33ab05de96c81ea02b1d1c4fde
Simone Sala <simonesala@me.com>
2020-05-19 15:29:22 +0200

    Database creation and posts_table

---
+++ database/migrations/2020_05_19_132411_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 150);
+            $table->string("slug");
+            $table->longText("body";)
+            $table->string("author");
+            $table->text("img_path");
+            $table->boolean("published");
+            $table->dateTime("published_at");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
